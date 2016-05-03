ed5428a9480fbbbdaf89b268162a69c99c897fdf
Andrey Savka <a_sawka@yahoo.com>
2016-05-03 08:43:34 +0300

    update folders structure

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var defaultLocals = require('./my_modules/default-locals.js');
+var defaultLocals = require('./modules/default-locals.js');
 
 var routes = require('./routes/index');
 
